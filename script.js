fb5ce6c22d47f7b109d92b7ac21d735ae546589d
Emile Mouannes <emile.mouannes@outlook.com>
2022-06-17 21:44:32 -0400

    Refine part of code

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 // Declarations
-
-
 let add = (a, b) => a + b;
 let substract = (a, b) => a - b;
 let multiply = (a, b) => a * b;
@@ -21,7 +19,9 @@ let memory = {
     storedOperator: "",
 }
 
+// Start calculator
 listenButtons();
+
 
 // Listen to input from user
 function listenButtons() {
@@ -31,19 +31,22 @@ function listenButtons() {
 
 function buttonPress(e, memory) {
     let input = e.target.innerText;
-    // if clear is selected
+    
+    // If clear is selected
     if (input == "AC") {
         memory.currentNumber = "";
         memory.previousNumber = "";
         memory.storedOperator = ""
     } else if (input == "C") {
         memory.currentNumber = memory.currentNumber.slice(0, -1);
-    // if a number is selected
+    
+    // If a number is selected
     } else if (input == "." && !(/[.]/g.test(memory.currentNumber))) {
         memory.currentNumber += input;
     } else if (/[0-9]/.test(input)) {
         memory.currentNumber += input;
-    // if an operator is selector
+    
+    // If an operator is selected
     } else if (/[=\-\+/x]/.test(input)) {
         if (!((memory.currentNumber == "" && memory.previousNumber == "") || (memory.currentNumber !== "" && memory.previousNumber !== "" && memory.storedOperator == "="))) {
             if (memory.currentNumber == "" && memory.previousNumber !== "") {
@@ -56,13 +59,9 @@ function buttonPress(e, memory) {
                 memory.previousNumber = operate(parseFloat(memory.previousNumber), parseFloat(memory.currentNumber), memory.storedOperator)
                 memory.storedOperator = input;
                 memory.currentNumber = "";
-            }
-        }
-    }
+    }}}
     display.update()
 }
-
-
 
 // Returns the calculated value
 function operate(firstValue, secondValue, operator) {
@@ -75,5 +74,3 @@ function operate(firstValue, secondValue, operator) {
     } else if (operator == "/") {
         return divide(firstValue, secondValue);
 }}
-
-
